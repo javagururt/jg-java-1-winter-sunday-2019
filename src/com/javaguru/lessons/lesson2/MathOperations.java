fea436b13903662e1f67ae53756bd87a8b27e147
Ruslans Tufetulovs <ruslans.tufetulovs@gmail.com>
2019-02-03 11:19:00 +0200

    math operations

---
+++ src/com/javaguru/lessons/lesson2/MathOperations.java
@@ -0,0 +1,16 @@
+package com.javaguru.lessons.lesson2;
+
+public class MathOperations {
+
+    public static void main(String[] args) {
+        int firstNumber = 10;
+        int secondNumber = 7;
+        int sumResult = firstNumber + secondNumber;
+        System.out.println("Sum result: " + sumResult);
+
+        double thirdNumber = 8.12;
+        double fourthNumber = 3.14;
+        double divideResult = thirdNumber / fourthNumber;
+        System.out.println("Divide result: " + divideResult);
+    }
+}
